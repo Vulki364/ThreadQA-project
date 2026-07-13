58d4ec529034d308f6588f4795199bb23430c2c5
olegzdanov34-vulki <olegzdanov34@gmail.com>
2026-07-13 19:47:46 +0500

    add tests class for test jenkins

---
+++ src/test/java/tests/RegistrationTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import core.BaseTest;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationTest extends BaseTest {
+
+    @Test
+    void testRegistration() {
+        System.out.println("Тест запустился");
+    }
+}
