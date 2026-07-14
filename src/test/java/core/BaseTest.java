820a7a9bf08fdd434f23d26a7e3180fd7e2eb66b
olegzdanov34-vulki <olegzdanov34@gmail.com>
2026-07-14 14:57:33 +0500

    Change BaseTest for Jenkins

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -4,14 +4,27 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
     @BeforeEach
     public void setUp() {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.setProperty(
+                    "webdriver.chrome.driver",
+                    "src/test/resources/drivers/chromedriver.exe");
+        }
         Configuration.browser = "chrome";
         Configuration.browserSize ="1920x1080";
-        Configuration.headless = false;
+        Configuration.headless = true;
+
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments("--headless=new")
+                .addArguments("--no-sandbox")
+                .addArguments("--disable-dev-shm-usage")
+                .addArguments("--disable-gpu");
+
     }
 
     @AfterEach
